4313bc859a973542a7e6320c6d08e2850891ee5a
Aa-Ff <x14562573449@gmail.com>
2019-09-24 21:41:26 +0800

    add some comments

--- src/main/java/problems/sort/MergeSort.java
+++ src/main/java/problems/sort/MergeSort.java
@@ -1,67 +1,68 @@
 package problems.sort;
 
 /**
- * 归并排序
+ * 归并排序。时间复杂度为O(nlogn), 空间复杂度是O(n)。
  */
-public class  MergeSort {
-    // 递归调用函数
-    private static void mergeSortInternally(int[] a, int p, int r) {
-        // 递归终止条件
-        if (p >= r) return;
-        // 取p到r之间的中间位置q,防止（p+r）之和超过int类型最大值
-        int q = p + (r - p)/2;
+public class MergeSort {
+	// 递归调用函数
+	private static void mergeSortInternally(int[] a, int left, int right) {
+		// 递归终止条件
+		if (left >= right) return;
+		// 取left到right之间的中间位置q,防止（left+right）之和超过int类型最大值
+		int q = left + (right - left) / 2;
 
-        // 分治递归
-        mergeSortInternally(a, p, q);
-        mergeSortInternally(a, q+1, r);
+		//分治递归
+		mergeSortInternally(a, left, q);
+		mergeSortInternally(a, q + 1, right);
 
-        merge(a, p, r, q);
-    }
+		merge(a, left, right, q);
+	}
 
-    //将排好序的数组合并
-    public static void merge(int[] a, int p, int r, int q) {
-        // 将A[p...q]和A[q+1...r]合并为A[p...r]
-        int i = p;
-        int j = q+1;
-        int k = 0; // 初始化变量i, j, k
-        int[] tmp = new int[r-p+1]; // 申请一个大小跟a[p...r]一样的临时数组
-        while (i<=q && j<=r) {
-            if (a[i] <= a[j]) {
-                tmp[k] = a[i]; // i++等于i:=i+1
-                k++;
-                i++;
-            } else {
-                tmp[k] = a[j];
-                k++; j++;
-            }
-        }
+	//将排好序的数组合并
+	public static void merge(int[] a, int left, int right, int q) {
+		// 将A[left...q]和A[q+1...right]合并为A[left...right]
+		int i = left;
+		int j = q + 1;
+		int k = 0; // 初始化变量i, j, k
+		//申请一个大小跟a[left...right]一样的临时数组
+		int[] tmp = new int[right - left + 1];
+		while (i <= q && j <= right) {
+			if (a[i] <= a[j]) {
+				tmp[k] = a[i];
+				k++;
+				i++;
+			} else {
+				tmp[k] = a[j];
+				k++;
+				j++;
+			}
+		}
 
-        // 判断哪个子数组中有剩余的数据
-        int start = i;
-        int end = q;
-        if (j <= r) {
-            start = j;
-            end = r;
-        }
+		//判断哪个子数组中有剩余的数据
+		//将剩余的数据拷贝到临时数组tmp
+		int start = i;
+		int end = q;
+		if (j <= right) {
+			start = j;
+			end = right;
+		}
+		while (start <= end) {
+			tmp[k++] = a[start++];
+		}
 
-        // 将剩余的数据拷贝到临时数组tmp
-        while (start <= end) {
-            tmp[k++] = a[start++];
-        }
+		// 将tmp中的数组拷贝回a[left...right]
+		for (i = 0; i <= right - left; ++i) {
+			a[left + i] = tmp[i];
+		}
+	}
 
-        // 将tmp中的数组拷贝回a[p...r]
-        for (i = 0; i <= r-p; ++i) {
-            a[p+i] = tmp[i];
-        }
-    }
+	public static void main(String[] args) {
+		int[] a = new int[]{80, 10, 120, 15, 1, 1};
+		int n = a.length;
+		mergeSortInternally(a, 0, n - 1);
 
-    public static void main(String[] args) {
-        int[] a = new int[]{80, 10, 120, 15, 1, 1};
-        int n = a.length;
-        mergeSortInternally(a, 0, n-1);
-
-        for (int i = 0; i < n; i++) {
-            System.out.println(a[i]);
-        }
-    }
+		for (int i = 0; i < n; i++) {
+			System.out.println(a[i]);
+		}
+	}
 }
