ba95432963613f6af3d90b5ebcf0f40eb7f7a3c9
aof00 <x14562573449@gmail.com>
2020-05-10 17:14:57 +0800

    bfs and dfs of graph

---
+++ src/main/java/algo_ds/tree/Graph.java
@@ -0,0 +1,27 @@
+package algo_ds.tree;
+
+import java.util.LinkedList;
+
+/**
+ * 图的实现
+ *
+ */
+public class Graph {
+	private int v; //顶点个数
+	private LinkedList<Integer>[] adj; //邻接表
+
+	public Graph(int v) {
+		this.v = v;
+		adj = new LinkedList[v];
+
+		for (int i = 0; i < v; i++) {
+			adj[i] = new LinkedList<>();
+		}
+	}
+
+	//无向图的一条边存两次
+	public void addEdge(int from, int to) {
+		adj[from].add(to);
+		adj[to].add(from);
+	}
+}
