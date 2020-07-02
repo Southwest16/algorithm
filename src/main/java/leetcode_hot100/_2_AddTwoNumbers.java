b045cfedc050f7a6f0254dc0fa839049b1196657
aof00 <x14562573449@gmail.com>
2020-07-02 23:17:52 +0800

    leetcode 15, 17, 19

--- src/main/java/leetcode_hot100/_2_AddTwoNumbers.java
+++ src/main/java/leetcode_hot100/_2_AddTwoNumbers.java
@@ -50,20 +50,3 @@ public class _2_AddTwoNumbers {
 	}
 }
 
-// Definition for singly-linked list.
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode() {
-	}
-
-	ListNode(int val) {
-		this.val = val;
-	}
-
-	ListNode(int val, ListNode next) {
-		this.val = val;
-		this.next = next;
-	}
-}
